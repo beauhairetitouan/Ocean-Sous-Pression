65215ced41b6a829685d8c4888e54760d46816d6
Titouan Beauhaire <titouan.beauhaire@etudiant.univ-lr.fr>
2025-01-23 13:49:25 +0100

    hash

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
