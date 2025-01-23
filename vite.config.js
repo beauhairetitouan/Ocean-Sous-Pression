8a1125f716626ebf6dcfba545635a6a15ee32d14
Titouan Beauhaire <titouan.beauhaire@etudiant.univ-lr.fr>
2025-01-23 14:26:42 +0100

    test

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// Remplacez 'ocean-sous-pression' par le nom de votre dépôt GitHub
-export default defineConfig({
-    plugins: [react()],
-    base: '/ocean-sous-pression/', // Chemin de base pour GitHub Pages
-});
